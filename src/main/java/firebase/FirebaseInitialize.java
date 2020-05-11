7ed39c20ab597b5c47534092ea6ac10f5d15def5
Frederik <frederik.moltke@hotmail.com>
2020-05-11 19:45:47 +0200

    webapp brugergrænseflade redesign, highscoreliste virker

--- src/main/java/firebase/FirebaseInitialize.java
+++ src/main/java/firebase/FirebaseInitialize.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,8 +12,6 @@ public class FirebaseInitialize {
     public void initialize(){
         try{
             InputStream fireKey = getClass().getResourceAsStream("/firekey.json");
-//            FileInputStream fireKey =
-//                    new FileInputStream("./firekey.json");
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(fireKey))
                     .setDatabaseUrl("https://backendapptetris-b186f.firebaseio.com")
